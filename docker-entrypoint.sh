4615697fb88877c945b671cfde266f0e531f319f
danielguerra69 <daniel.guerra69@gmail.com>
2017-10-10 10:33:48 +0200

    startup fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #! /bin/sh
 
 set -e
+echo "sdk" | sudo -S chown -R sdk:sdk /home/sdk
 if [ -d "/home/sdk/.abuild" ]; then
     echo "sdk" | sudo -S cp ~/.abuild/*.pub /etc/apk/keys
 else
+	mkdir ~/.abuild
     abuild-keygen -a -n
     echo "sdk" | sudo -S cp ~/.abuild/*.pub /etc/apk/keys
 fi
